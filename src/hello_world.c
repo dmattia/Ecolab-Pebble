557b0d3cd5d26188e426a9679c2b72b9f41f868c
David Mattia <dmattia@nd.edu>
2015-05-11 23:28:20 -0500

    Added working weather widget

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,7 +1,12 @@
 #include <pebble.h>
 
+enum {
+  KEY_TEMPERATURE = 0,
+  KEY_CONDITIONS = 1
+};
+	
 Window *window;
-static TextLayer *s_text_layer;
+static TextLayer* s_text_layer;
 static TextLayer* s_time_layer;
 static TextLayer* s_weather_layer;
 
@@ -30,11 +35,66 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 	update_time();
 }
 
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+  
+  // Read first item
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    case KEY_TEMPERATURE:
+      snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)t->value->int32);
+      break;
+    case KEY_CONDITIONS:
+      snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+      break;
+    default:
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
+    }
+
+    // Look for next item
+    t = dict_read_next(iterator);
+  }  
+	
+  // Assemble full string and display
+  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+  text_layer_set_text(s_weather_layer, weather_layer_buffer);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 void handle_init(void) {
+	// Register callbacks
+	app_message_register_inbox_received(inbox_received_callback);
+	app_message_register_inbox_dropped(inbox_dropped_callback);
+	app_message_register_outbox_failed(outbox_failed_callback);
+	app_message_register_outbox_sent(outbox_sent_callback);
+	
+	 // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+	
 	// Create a window and other layers
 	window = window_create();
 	s_text_layer = text_layer_create(GRect(0, 0, 144, 154));
 	s_time_layer = text_layer_create(GRect(0,40,144,114));
+	s_weather_layer = text_layer_create(GRect(0, 80, 144, 74));
 	
 	// Register with TickTimerService
 	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
@@ -51,9 +111,16 @@ void handle_init(void) {
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 	text_layer_set_background_color(s_time_layer, GColorClear);
 	
+	// Setup for weather layer
+	text_layer_set_text(s_weather_layer, "...");
+	text_layer_set_font(s_weather_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+	text_layer_set_background_color(s_weather_layer, GColorClear);
+	
 	// Add layers to window
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_text_layer));
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
 
 	// Push the window
 	window_stack_push(window, true);
@@ -65,6 +132,7 @@ void handle_init(void) {
 void handle_deinit(void) {
 	text_layer_destroy(s_time_layer);
 	text_layer_destroy(s_text_layer);
+	text_layer_destroy(s_weather_layer);
 	window_destroy(window);
 }
 
